b9d6e10f0273f6317e8f88e37fd6b55a0e446f8c
Anas CHERIET <anascheriet@hotmail.com>
2021-01-10 16:08:56 +0100

    Login Logo and description

--- src/Components/auth/login.js
+++ src/Components/auth/login.js
@@ -1,12 +1,23 @@
+import { Form, Input } from 'antd'
 import React from 'react'
 import "./login.scss"
 
 export const Login = () => {
-    return (
-      <div className="parent">  
-        <div className="child">
-
+  return (
+    <div className="container">
+      <div className="logo">
+        <div className="name">
+          <h2>eventor</h2>
         </div>
+        <div className="description">
+          <h2>EVENT PLANNING.</h2>
+        </div>
+
+      </div>
+
+      <div className="loginForm">
+
+      </div>
     </div>
-    )
+  )
 }
