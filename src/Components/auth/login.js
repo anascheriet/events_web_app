fb51e826d57bb42f5ac292ca6d195c04f21f0ee4
Anas CHERIET <anascheriet@hotmail.com>
2021-01-10 20:45:33 +0100

    Login Page Design Done

--- src/Components/auth/login.js
+++ src/Components/auth/login.js
@@ -1,11 +1,12 @@
-import { Form, Input } from 'antd'
+import { Button, Input, Form } from 'antd'
 import React from 'react'
 import "./login.scss"
 
 export const Login = () => {
+
   return (
     <div className="container">
-      <div className="logo">
+      <div className="logo-container">
         <div className="name">
           <h2>eventor</h2>
         </div>
@@ -15,9 +16,30 @@ export const Login = () => {
 
       </div>
 
-      <div className="loginForm">
 
+      <div className="form">
+        <form >
+          <label className="label">USERNAME</label>
+          <br />
+          <input type="text" className="input" />
+          <br />
+          <br />
+          <label className="label">PASSWORD</label>
+          <br />
+          <input type="password" className="input" />
+          <br />
+          <br />
+
+          <button className="submitBtn">Log In</button>
+
+        </form>
+        <div className="links">
+          <a  href="">Create Account</a>
+          <a  href="">Forgot Password?</a>
+        </div>
       </div>
+
     </div>
-  )
+  );
+
 }
