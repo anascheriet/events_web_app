fe29879a9c7fcfc72950ab377f711cb1e499af9b
Anas CHERIET <anascheriet@hotmail.com>
2021-01-29 20:44:25 +0100

    Purge Css attempt 2

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,5 +1,15 @@
 const tailwindcss = require("tailwindcss");
 
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        './src/**/*.js',
+        './public/index.html'
+    ],
+    css: ['./src/styles.css'],
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
-    plugins: [tailwindcss('./tailwind.config.js', require('autoprefixer'))]
+    plugins: [tailwindcss('./tailwind.config.js', require('autoprefixer')), purgecss]
 }
