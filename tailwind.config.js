1eee5d7e6da7b87eafb0594d3f47eb2cb0bd05e2
Anas CHERIET <anascheriet@hotmail.com>
2021-01-29 20:36:29 +0100

    TailwindCss Purge Attempt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.js'
+  ],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
